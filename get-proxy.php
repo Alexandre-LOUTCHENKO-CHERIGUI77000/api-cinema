7903d9f8c36192e322189c591eb09e3daa6cfdfd
Alexandre-LOUTCHENKO-CHERIGUI77000 <alexandre.loutchenko@gmail.com>
2026-04-18 15:48:33 +0200

    Ajout movie.php, actor.php, autocomplete.php et corrections

--- get-proxy.php
+++ get-proxy.php
@@ -1,20 +1,25 @@
 <?php
-function getProxy($url){
-    $options = [
-        'http' => [
-            'proxy' => 'tcp://172.16.0.54:8080',
-            'request_fulluri' => true,
-        ],
-    ];
-    $context = stream_context_create($options);
+function getProxy($url) {
+    // Essai direct (chez soi)
+    $response = @file_get_contents($url);
 
-    $response = file_get_contents($url, false, $context);
+    // Si ça échoue, on essaie via le proxy du lycée
+    if ($response === false) {
+        $options = [
+            'http' => [
+                'proxy' => 'tcp://172.16.0.54:8080',
+                'request_fulluri' => true,
+            ],
+        ];
+        $context = stream_context_create($options);
+        $response = @file_get_contents($url, false, $context);
+    }
 
     if ($response === false) {
         echo "Failed to get data from $url";
-    } else {
-        return $response;
+        return null;
     }
-}
 
+    return $response;
+}
 ?>
